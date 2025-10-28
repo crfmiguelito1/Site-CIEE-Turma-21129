ae98eccbca00800c30961b7b5671b6c34069a76e
Miguel <azirando77@gmail.com>
2025-10-27 23:48:19 -0300

    Add initial project files including web manifest, package configuration, and main HTML structure for the dashboard and training module

---
+++ JS/dados.js
@@ -0,0 +1,86 @@
+// Arquivo: dados.js
+
+// Nossa base de dados simulada.
+const baseDeDados = {
+  cursos: [
+    {
+      id: 1,
+      nome: "Capacitação",
+      nivel: "Iniciante",
+      duracao: 10,
+      icone: "fa-solid fa-graduation-cap",
+      inscritos: [
+        { nome: "João Silva", progresso: 100 },
+        { nome: "Mariana Lima", progresso: 85 },
+        { nome: "Carlos Souza", progresso: 90 },
+      ],
+    },
+    {
+      id: 2,
+      nome: "Padronização de Processos",
+      nivel: "Intermediário",
+      duracao: 15,
+      icone: "fa-solid fa-gear",
+      inscritos: [
+        { nome: "Ana Costa", progresso: 75 },
+        { nome: "Pedro Mendes", progresso: 60 },
+      ],
+    },
+    {
+      id: 3,
+      nome: "Adaptação à Tecnologia",
+      nivel: "Avançado",
+      duracao: 20,
+      icone: "fa-solid fa-computer",
+      inscritos: [
+        { nome: "João Silva", progresso: 40 },
+        { nome: "Beatriz Almeida", progresso: 55 },
+      ],
+    },
+     {
+      id: 4,
+      nome: "Aprendizado Contínuo",
+      nivel: "Avançado",
+      duracao: 20,
+      icone: "fa-solid fa-arrows-rotate ",
+      inscritos: [
+        { nome: "João Silva", progresso: 40 },
+        { nome: "Beatriz Almeida", progresso: 55 },
+      ],
+    },
+     {
+      id: 5,
+      nome: "Evitar Falhas",
+      nivel: "Avançado",
+      duracao: 20,
+      icone: "fa-solid fa-triangle-exclamation",
+      inscritos: [
+        { nome: "João Silva", progresso: 40 },
+        { nome: "Beatriz Almeida", progresso: 55 },
+      ],
+    },
+     {
+      id: 6,
+      nome: "Produtividade",
+      nivel: "Avançado",
+      duracao: 20,
+      icone: "fa-solid fa-bolt",
+      inscritos: [
+        { nome: "João Silva", progresso: 40 },
+        { nome: "Beatriz Almeida", progresso: 55 },
+      ],
+    },
+     {
+      id: 7,
+      nome: "Reduzir Rotatividade",
+      nivel: "Avançado",
+      duracao: 20,
+      icone: "fa-solid fa-handshake ",
+      inscritos: [
+        { nome: "João Silva", progresso: 40 },
+        { nome: "Beatriz Almeida", progresso: 55 },
+      ],
+    }
+    // Adicione os outros cursos aqui no mesmo formato...
+  ],
+};
